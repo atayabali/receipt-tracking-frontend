0006e52142731ce2bf94ad97245308a0c5fd707a
Adam Tayabali <adamtayabali@gmail.com>
2025-02-13 13:01:30 -0600

    remove code

--- app/(tabs)/addEntry.tsx
+++ app/(tabs)/addEntry.tsx
@@ -1,19 +1,9 @@
 import { styles } from "@/assets/globalStyles";
-import { View, Text } from "@/components/Themed";
+import { View } from "@/components/Themed";
 import Title from "@/components/Title";
 import React, { useEffect } from "react";
-import { Platform } from "react-native";
 import FormikContainer from "@/components/Forms/FormikContainer";
-var localhost = Platform.OS === "web" ? "localhost" : "10.0.0.101"; // "192.168.0.86";
-var urlPrefix = `http://${localhost}:5000`;
 
-// const fetchExpenses = async (): Promise<Expense[]> => {
-//   const response = await F.get<Expense[]>(
-//     `${urlPrefix}/api/v1/expenses/all`
-//   );
-//   console.log(response);
-//   return response.data;
-// };
 export default function AddExpense() {
   // const router = useRouter();
   //api request works but don't want to call server every time right now so hardcoded
