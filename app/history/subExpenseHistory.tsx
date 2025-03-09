8299eca53e1f538de5b1f1ed167df820f4bab33b
Adam Tayabali <adamtayabali@gmail.com>
2025-03-09 15:56:53 -0500

    code clean up and useAws

--- app/history/subExpenseHistory.tsx
+++ app/history/subExpenseHistory.tsx
@@ -1,76 +1,68 @@
 import { styles } from "@/assets/globalStyles";
 import { Text, View } from "@/components/Themed";
-import { deleteExpense, fetchExpenses } from "@/services/expenseService";
-import { useRouter } from "expo-router";
-import React, { useCallback, useEffect, useState } from "react";
-import { useFocusEffect } from "@react-navigation/native";
-import ExpenseTableV2 from "@/components/ExpenseTable";
-import { Expense } from "@/models/Expense";
-import ConfirmCancelAlert from "@/components/Alerts/ConfirmCancelAlert";
-import GreenOutlineBtn from "@/components/GreenOutlineBtn";
-import { fetchAllSubItemsSearchOptional } from "@/services/subexpenseService";
 import { SubExpenseHistoryItem } from "@/models/SubItem";
+import { fetchAllSubItemsSearchOptional } from "@/services/subexpenseService";
+import { useFocusEffect } from "@react-navigation/native";
 import { SearchBar } from "@rneui/themed";
-
-import { Cell, Table, TableWrapper } from "react-native-table-component";
+import { useRouter } from "expo-router";
+import React, { useCallback, useState } from "react";
 import { TableHeader } from "@/components/Cells/TableHeader";
+import _ from "lodash";
 import { ScrollView } from "react-native";
-import _ from 'lodash';
+import { Cell, Table, TableWrapper } from "react-native-table-component";
 
 export default function SubExpenseHistory() {
   const router = useRouter();
   const [subexpenses, setSubExpenses] = useState<SubExpenseHistoryItem[]>([]);
-  const [search, setSearch] = useState(''); 
-  //   const [dateOrder, setDateOrder] = useState("desc");
-  const [refresh, setRefresh] = useState(true);
-
+  const [search, setSearch] = useState("");
 
   const getSubExpenses = async (query: string) => {
     fetchAllSubItemsSearchOptional(query)
       .then(setSubExpenses)
       .catch(console.error);
-    setRefresh(false);
   };
 
-//   const handler = useCallback(debounce(getSubExpenses, 2000), []);
-const debouncedGetSubExpenses = useCallback(_.debounce((query) => getSubExpenses(query), 1000), []);
+  //   useCallback ensures debouncedFetch remains stable across re-renders.
+  //   _.debounce delays execution until after 1 second of inactivity.
+  //   Only the last API call executes, canceling previous calls if a new character is typed within the debounce delay.
+  const debouncedGetSubExpenses = useCallback(
+    _.debounce((query) => getSubExpenses(query), 1000),
+    []
+  );
 
   const onSearch = (val: string) => {
-      setSearch(val);
-      debouncedGetSubExpenses(val);
-    //   getSubExpenses();
-    //   useCallback(_.debounce(getSubExpenses, 2000), []);
-   };
+    setSearch(val);
+    debouncedGetSubExpenses(val);
+  };
 
-  useEffect(() => {
-    if (refresh) {
-      getSubExpenses(search);
-    }
-  }, [refresh]); // The function only runs when property is true
-
-  // https://reactnavigation.org/docs/use-focus-effect/
   //useFocusEffect - to run side-effects when a screen is focused. A side effect may involve things like adding an event listener, fetching data,
   useFocusEffect(
     useCallback(() => {
-      getSubExpenses(search)
+      getSubExpenses(search);
     }, [])
   );
 
   return (
     <View style={styles.tableContainer}>
-        <SearchBar 
+      <SearchBar
         placeholder="Search for item"
         onChangeText={onSearch}
-        value={search}/>
-        <Text>Retrieving {search.length > 0 ? `items that contains '${search}'` : "all items"}</Text>
-        <ScrollView>
-          <Table borderStyle={styles.tableBorder}>
-            <TableHeader columnNames={['Grocery Item', 'Cost', 'Date']} />
-            {subexpenses.map((subItem: SubExpenseHistoryItem) => [
-                  subItem.name,
-                  subItem.cost,
-                  subItem.date,
-                ]).map((rowData: any, index: number) => (
+        value={search}
+      />
+      <Text>
+        Retrieving{" "}
+        {search.length > 0 ? `items that contains '${search}'` : "all items"}
+      </Text>
+      <ScrollView>
+        <Table borderStyle={styles.tableBorder}>
+          <TableHeader columnNames={["Grocery Item", "Cost", "Date"]} />
+          {subexpenses
+            .map((subItem: SubExpenseHistoryItem) => [
+              subItem.name,
+              subItem.cost,
+              subItem.date,
+            ])
+            .map((rowData: any, index: number) => (
               <TableWrapper key={index} style={styles.tableRow}>
                 {rowData.map((cellData: any, cellIndex: number) => (
                   <Cell
@@ -81,8 +73,8 @@ const debouncedGetSubExpenses = useCallback(_.debounce((query) => getSubExpenses
                 ))}
               </TableWrapper>
             ))}
-          </Table>
-          </ScrollView>
+        </Table>
+      </ScrollView>
     </View>
   );
 }
