7f07a006f295335470d4030c6bc6a567c3d9d585
Adam Tayabali <adamtayabali@gmail.com>
2025-03-25 21:59:09 -0500

    useAuth in add entry page

--- app/(tabs)/addEntry.tsx
+++ app/(tabs)/addEntry.tsx
@@ -3,8 +3,14 @@ import { View } from "@/components/Themed";
 import Title from "@/components/Title";
 import React from "react";
 import FormikContainer from "@/components/Forms/FormikContainer";
+import { useAuth } from "@/services/authContext";
+import { setAccessToken } from "@/services/api";
 
 export default function AddExpense() {
+    const { accessToken } = useAuth(); // Get token from context
+  
+    // Set the token provider dynamically
+    setAccessToken(accessToken);
   return (
     <View style={styles.container}>
       <Title title="Manual Expense Entry"></Title>
